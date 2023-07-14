eeeb09bf0b1f95d5ee5363e32c3909e284f1a411
Rae Stanton <raestanton93@gmail.com>
2023-07-13 20:14:47 -0700

    loop lab
    This is a mix of the code along challenge and the tested portion of the looping lab.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+  var messages = [];
+  for (var i = 0; i < names.length; i++) {
+    var message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    messages.push(message);
+  }
+  return messages;
+}
+
+var names = ["Charlie", "Samip", "Ali"];
+var eventName = "birthday";
+var result = writeCards(names, eventName);
+console.log(result);
+
+// second portion to be tested
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+countDown(10);
